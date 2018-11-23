59770c086ccf03cc0ca7474cb733088bd9eb1949
Lucas Yu <wdmx666@sina.cn>
2018-11-23 19:15:34 +0800

    6

---
+++ spark/sp.py
@@ -0,0 +1,21 @@
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.classification import LogisticRegression
+from pyspark.sql import SparkSession
+# Prepare training data from a list of (label, features) tuples.
+import os
+#os.environ["HADOOP_HOME"] = r"F:\environment\runtime\hadoop-2.7.1"
+spark = SparkSession.builder.master("local[3]").appName('abc').getOrCreate()
+
+training = spark.createDataFrame([
+    (1.0, Vectors.dense([0.0, 1.1, 0.1])),
+    (0.0, Vectors.dense([2.0, 1.0, -1.0])),
+    (0.0, Vectors.dense([2.0, 1.3, 1.0])),
+    (1.0, Vectors.dense([0.0, 1.2, -0.5]))], ["label", "features"])
+
+# Create a LogisticRegression instance. This instance is an Estimator.
+lr = LogisticRegression(maxIter=10, regParam=0.01)
+# Print out the parameters, documentation, and any default values.
+print("LogisticRegression parameters:\n" + lr.explainParams() + "\n")
+
+# Learn a LogisticRegression model. This uses the parameters stored in lr.
+model1 = lr.fit(training)
